75350fc5813e11fd22ede0bad17e3741a70dbc44
Chrissy <c.atherholt@gmail.com>
2019-09-21 14:55:27 -0400

    added validation, made some final changes, app is ready to go

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,150 +1,116 @@
-// Initial array of topics
-var topics = [
-    "Tina Fey",
-    "Amy Poehler",
-    "Kristen Wiig",
-    "Kate McKinnon",
-    "Aubrey Plaza",
-    "Maya Rudolph",
-    "Ellie Kemper",
-    "Mindy Kaling",
-    "Emma Stone",
-    "Lisa Kudrow",
-    "Catherine O'Hara"
-];
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB74O4LPjEB3PXYgozNGAEUpMAOmdBvkb8",
+    authDomain: "manifest-ivy-149822.firebaseapp.com",
+    databaseURL: "https://manifest-ivy-149822.firebaseio.com",
+    projectId: "manifest-ivy-149822",
+    storageBucket: "manifest-ivy-149822.appspot.com",
+    messagingSenderId: "687451388117",
+    appId: "1:687451388117:web:f479741018f53d54beca5c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Create a variable to reference the database
+var database = firebase.database();
 
-// Variables related to the GiphY API
+// Check if the military time is entered correctly
+function validateHhMm(inputField) {
+var isValid = /^([0-1][0-9]|2[0-3]):([0-5][0-9])$/.test(inputField.value);
 
-var APIkey = "q9CGcggx5BEpVZZ63u4a2I817X5LVIvy";
-var sampleURL = "http://api.giphy.com/v1/gifs/search?q=SNL&api_key=q9CGcggx5BEpVZZ63u4a2I817X5LVIvy&limit=5";
-
-
-// Function for displaying the gifs
-function displayGifs() {
-
-    var category = $(this).attr("data-name");
-
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + category + "&api_key=" + APIkey + "&limit=10";
-
-
-    // Creating an AJAX call for the specific category button being clicked
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(queryURL);
-        console.log(response);
-
-        $("#gifs-view").empty();
-
-        for (var i = 0; i < response.data.length; i++) {
-
-            // Check to see that the correct data is being pulled
-            console.log(response.data[i].rating);
-            console.log(response.data[i].images.original.url);
-            console.log(response.data[i].images.original_still.url);
-
-            // Create local variables for that data
-            var rating = response.data[i].rating;
-            // Fixed height so they fit together nicely
-            var stillGif = response.data[i].images.fixed_height_still.url;
-            var animatedGif = response.data[i].images.fixed_height.url;
-
-            // Create a well div for the image and the rating
-            var wellDiv = $("<div>");
-            wellDiv.addClass("card");
-            wellDiv.attr("id", "articleDiv-" + i);
-            $("#gifs-view").append(wellDiv);
-
-            // Attach the content to the appropriate well
-            if (response.data[i].rating != "null") {
-
-                // create a <p> tag for the Rating info
-                var pCard = $("<p>");
-                pCard.addClass("card-text");
-                pCard.text("Rating: " + rating);
-
-                $("#articleDiv-" + i).append(pCard);
-            }
-
-            // Create an image tag for each of the gifs with all the different attributes
-            var gifImg = $("<img>");
-            gifImg.addClass("gif");
-            gifImg.addClass("card-img-top");
-            gifImg.attr("data-state", "still");
-            gifImg.attr("data-still", stillGif);
-            gifImg.attr("data-animate", animatedGif);
-            gifImg.attr("src", stillGif);
-
-            // Attach the gifs to the divs on the webpage
-            $("#articleDiv-" + i).prepend(gifImg);
-        }
-    });
+if (isValid) {
+    inputField.style.backgroundColor = '#bfa';
+    $("#submit").attr("disabled", false);
+} else {
+    inputField.style.backgroundColor = '#fba';
+    $("#alert").text("Please enter the time in military time")
 }
 
-// Function for changing gifs from still to animated
-$(document).on("click", ".gif", function () {
-
-    // Local variables that collect the attribute data
-    var state = $(this).attr("data-state");
-    var animateVal = $(this).attr("data-animate");
-    var stillVal = $(this).attr("data-still");
-
-    // Check the variable's data state and then switch it from still to animate and change the src attribute from still to animate
-    if (state === "still") {
-        $(this).attr({
-            src: animateVal,
-            "data-state": "animate"
-        });
-    } else {
-        $(this).attr({
-            src: stillVal,
-            "data-state": "still"
-        });
-    }
-});
-
-
-// Function for displaying buttons
-function renderButtons() {
-
-    // Deleting the category buttons prior to adding new category buttons
-    $("#buttons-view").empty();
-
-    // Looping through the array of topics
-    for (var i = 0; i < topics.length; i++) {
-
-        // Code that creates the HTML that is added to the webpage
-        var newBtn = $("<button>");
-        newBtn.addClass("topic-btn btn");
-        newBtn.attr("data-name", topics[i]);
-        newBtn.text(topics[i]);
-
-        // add that new HTML to the buttons section of the webpage
-        $("#buttons-view").append(newBtn);
-    }
+return isValid;
 }
 
-// Function for adding new topic buttons
-$("#add-topic").on("click", function (event) {
-
-    // prevent page from reloading
+// Capture Button Click
+$("#submit").on("click", function (event) {
+    // prevent page from refreshing when form tries to submit itself
     event.preventDefault();
 
-    // This grabs the new topic
-    var newTopic = $("#topic-input").val().trim();
-    // then adds the topic to the array
-    topics.push(newTopic);
+    // Capture user inputs and store them into variables
+    var trainName = $("#train-name").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrainTime = $("#first-train-time").val().trim();
+    var frequency = $("#frequency").val().trim();
 
-    // this regenerates all the buttons from the array (instead of just adding a new one)
-    renderButtons();
+    // Code for handling the push
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
 
     // Clear previous input
-    $("#topic-input").val("");
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train-time").val("");
+    $("#frequency").val("");
+    $("#submit").attr("disabled", true);
+
 });
 
-// When you click a button, that loads all the gifs from the function above
-$(document).on("click", ".topic-btn", displayGifs);
+// Firebase watcher .on("child_added"
+database.ref().on("child_added", function (snapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
 
-// This function needs to be called when the page loads to load the buttons in the intial array
-renderButtons();
+    // Console log each of the user inputs to confirm we are receiving them
+    console.log(sv.trainName);
+    console.log(sv.destination);
+    console.log(sv.firstTrainTime);
+    console.log(sv.frequency);
+
+    // Some Math & Moment.js to determine train arrivals:
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(sv.firstTrainTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % sv.frequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = sv.frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrainLong = moment().add(tMinutesTillTrain, "minutes");
+    var nextTrain = moment(nextTrainLong).format("HH:mm");
+    console.log("ARRIVAL TIME: " + moment(nextTrainLong).format("HH:mm"));
+
+
+    // Create new HTML for this new train data
+    var newRow = $("<tr>");
+
+    var trainNameTd = $("<td>").text(sv.trainName);
+    var destinationTd = $("<td>").text(sv.destination);
+    var frequencyTd = $("<td>").text(sv.frequency);
+    var nextTrainTd = $("<td>").text(nextTrain);
+    var tMinutesTillTrainTd = $("<td>").text(tMinutesTillTrain);
+
+    newRow.append(trainNameTd, destinationTd, frequencyTd, nextTrainTd, tMinutesTillTrainTd);
+
+    // Append the table row to the table body
+    $("tBody").append(newRow);
+
+
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
